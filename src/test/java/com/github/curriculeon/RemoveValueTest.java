ca8bf64f4cbfa17b979305eb8c3472da0ad38244
Michal Terranova <michalterranova@mail.com>
2020-11-25 11:22:24 -0500

    Update RemoveValueTest.java

--- src/test/java/com/github/curriculeon/RemoveValueTest.java
+++ src/test/java/com/github/curriculeon/RemoveValueTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 
 public class RemoveValueTest {
 
-    private void test(String[] array, String[] expected) {
-        String[] actual = StringArrayUtils.removeValue(array, "The");
+    private void test(String[] array, String[] expected, String word) {
+        String[] actual = StringArrayUtils.removeValue(array, word);
         Assert.assertEquals(expected, actual);
     }
 
@@ -14,22 +14,26 @@ public class RemoveValueTest {
     public void testRemoveValue() {
         String[] array = {"The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
         String[] expected = {"quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
-        test(array, expected);
+        String word = "The";
+        test(array, expected, word);
     }
 
     @Test
     public void testRemoveValue1() {
         String[] array = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
         String[] expected = {"the", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
-        test(array, expected);
+        String word = "quick";
+        test(array, expected, word);
     }
 
     @Test
     public void testRemoveValue2() {
         String[] array = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
         String[] expected = {"the", "quick", "fox", "jumps", "over", "the", "lazy", "dog"};
-        test(array, expected);
+        String word = "brown";
+        test(array, expected, word);
     }
 
 
 }
+
